f3be7cbc0ea2cd778d8c8186d8bde9ddbdb2f8a9
Ness Hm <nessmahamidou92@gmail.com>
2024-12-12 10:58:48 +0100

    test function

--- calculator.js
+++ calculator.js
@@ -16,6 +16,9 @@ export function divide(a, b) {
     if (b === 0) {
         throw new Error('Cannot divide by zero');
     }
-
-    return a / b;
+    /*
+    if (typeof b ==='string')
+    return  eval(b)
+     */
+    return a / b
 }
